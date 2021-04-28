6a8f33ebf6ccb1832ce1227712369f7d1b74de0c
Drew Haines <drewhaines3@gmail.com>
2021-04-28 01:01:37 -0700

    more changes to about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -19,12 +19,12 @@ const About = () => {
         overflow: "auto"
       }}
       px={[3, null, null, 10]}
-      pb={[3, null, null, 10]}
+      pb={[8, null, null, 10]}
     >
-      <Grid variant="centered" mt={10}>
+      <Grid variant="centered" mt={[5, null, null, 10]}>
         <Container>
           <Heading variant="h1" as="h1" sx={{ textAlign: "center" }}>
-            What is The Dev Experience?
+            What Is The Dev Experience?
           </Heading>
           <Grid
             columns={["1fr", null, null, "1fr 1fr"]}
@@ -32,15 +32,25 @@ const About = () => {
             pt={[5, 10]}
             mb={0}
           >
-            <Text sx={{ fontSize: 28, lineHeight: "42px" }}>
-              {`The Dev Experience is a place to learn web development. 
+            <Text
+              sx={{
+                fontSize: ["22px", null, null, "24px"],
+                lineHeight: ["24px", null, null, "28px"]
+              }}
+            >
+              {`The Dev Experience is a place to learn web development online. 
 
-I've created a curated list of resources to learn software development from junior to senior level topics. It's a community where we can support each other and connect with other developers to make our lives better.
+It's a curated list of resources that range from junior to senior level topics. This is a community where we can support each other and connect with other developers to make our lives better.
 
-I teach my specific development stack, tools, and methodology. This is exactly what I would have taught myself 10 years ago to set my career on track.`}
+I teach my specific development stack, tools, and methodology. This is exactly what I would have taught myself 10 years ago to set my career on the right track.`}
             </Text>
             <Box my={[3, 0]}>
-              <StaticImage src="../images/coding.png" alt="table with code" />
+              <Box sx={{ "& > div": { maxWidth: 220, borderRadius: "10px" } }}>
+                <StaticImage
+                  src="../images/tde_logo.png"
+                  alt="table with code"
+                />
+              </Box>
             </Box>
           </Grid>
           <Heading
@@ -49,7 +59,7 @@ I teach my specific development stack, tools, and methodology. This is exactly w
             mt={[10, 20]}
             sx={{ textAlign: "center" }}
           >
-            Why Are You Doing This?
+            Why Does It Exist?
           </Heading>
           <Grid
             columns={["1fr", null, null, "1fr 1fr"]}
@@ -57,12 +67,17 @@ I teach my specific development stack, tools, and methodology. This is exactly w
             pt={[5, 10]}
             mb={0}
           >
-            <Text sx={{ fontSize: 28, lineHeight: "34px" }}>
+            <Text
+              sx={{
+                fontSize: ["22px", null, null, "24px"],
+                lineHeight: ["24px", null, null, "28px"]
+              }}
+            >
               {`Software literally changed my life. 
 
-I went through a difficult time during college where I could barely make ends meet. When my car broke, I couldn't afford to have a mechanic fix it. I usually couldn't go out to bars with friends in college. There were times when I literally couldn't afford to eat lunch and would just go hungry.
+I went through a difficult time during college where I could barely make ends meet. When my car broke, I had to fix it myself because I couldn't afford a mechanic. I was embarrassed that I couldn't pay to go to events or trips with friends. There were times when I couldn't even afford to eat and would just go hungry.
 
-Now I'm doing great.
+Fast foward a few years and I'm doing way better.
 
 Learning software made it so I could make my own money and build a real career. It opened lots of doors for me and it can for you too. This is one of the best ways I can share my knowledge and give back to the community.`}
             </Text>
@@ -74,7 +89,7 @@ Learning software made it so I could make my own money and build a real career. 
             mt={[10, 20]}
             sx={{ textAlign: "center" }}
           >
-            A Little More About Me
+            A Little About Me
           </Heading>
           <Grid
             columns={["1fr", null, null, "1fr 1fr"]}
@@ -83,32 +98,43 @@ Learning software made it so I could make my own money and build a real career. 
             mb={0}
           >
             <Box my={[3, 0]}></Box>
-            <Text sx={{ fontSize: 28, lineHeight: "42px" }}>
-              {`I'm Drew and I run most things at here. 
+            <Text
+              sx={{
+                fontSize: ["22px", null, null, "24px"],
+                lineHeight: ["24px", null, null, "28px"]
+              }}
+            >
+              {`I'm Drew and I run most things at The Dev Experience. 
 
 I have over 10 years worth of software development experience. Most of my work is in HTML, CSS, JS and Ruby. But it always depends on what clients need or what project I'm working on.
 
 Ever since I started tutoring in college, I've felt a passion to teach and help others.`}
             </Text>
-            <Text sx={{ fontSize: 28, lineHeight: "42px" }}>
+            <Text
+              sx={{
+                fontSize: ["22px", null, null, "24px"],
+                lineHeight: ["24px", null, null, "28px"]
+              }}
+            >
               {`I'm married to Sea-Anna. 
 
 She also does software development.
 
-Sea-Anna actually changed her career from fitness/personal training to software. She went to LEARN Academy, for 4 months, worked for a startup for ~1 year+, and now works for Reviewed.com.
-
-She has a great story and may pop in from time to time.`}
+Sea-Anna changed her career from fitness/personal training to software. She went to LEARN Academy, for 4 months, worked for a startup for 1 year+, and has worked at Reviewed for over 3 years.`}
             </Text>
             <Box></Box>
             <Box></Box>
-            <Text sx={{ fontSize: 28, lineHeight: "42px" }}>
-              {`Aside from coding I have a few hobbies.  
+            <Text
+              sx={{
+                fontSize: ["22px", null, null, "24px"],
+                lineHeight: ["24px", null, null, "28px"]
+              }}
+            >
+              {`I love surfing. It's such a good mix of a physical and mental excercise. Reading waves helps heighten your awareness which is important in coding and business. Practicing good balance helps 
 
-I love surfing. It's such a good mix between physical and mental excercise. Reading waves helps heighten your awareness which is important in coding and business. Big surf is challenging and 
+Running and working out is also a big part of my life. Since we sit all day as software developers, it's important to get out of your chair and treat your body right.
 
-Running and working out is a big part of my life. Since we sit all day as software developers, it's important to get out of your chair and treat your body right.
-
-I love hanging with friends and family. The relationships we have are more important than we think.`}
+I unapologetically put stickers on all the things. Here are a few of my favorite sticker packs.`}
             </Text>
           </Grid>
           <Heading
@@ -126,7 +152,11 @@ I love hanging with friends and family. The relationships we have are more impor
             mb={0}
           >
             <Text
-              sx={{ fontSize: 28, lineHeight: "42px", px: [5, null, null, 20] }}
+              sx={{
+                fontSize: ["22px", null, null, "24px"],
+                lineHeight: ["24px", null, null, "28px"],
+                px: [5, null, null, 20]
+              }}
             >
               {`Life isn't all about coding and making money. 
               
